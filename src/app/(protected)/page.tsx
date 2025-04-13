07516f65bed971128f0a529e99d147c1b5b09407
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2025-04-14 04:47:57 +0530

    Enhance dashboard page layout with welcoming message
    - Updated the dashboard page to include a centered welcome message, improving user engagement and visual appeal.
    - Applied Tailwind CSS classes for better styling and responsiveness.

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full">
+      <h1 className="text-2xl font-bold">Welcome to the dashboard</h1>
+    </div>
+  );
 };
 
 export default page;
